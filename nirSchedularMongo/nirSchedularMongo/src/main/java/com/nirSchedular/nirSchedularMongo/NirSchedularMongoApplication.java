54d73031fd47ab5380fb25a5253a9e94337b9e0c
Itzhak Levy <itzhaklevydev@gmail.com>
2025-05-10 13:16:26 +0300

    first commit - creating the server

---
+++ nirSchedularMongo/nirSchedularMongo/src/main/java/com/nirSchedular/nirSchedularMongo/NirSchedularMongoApplication.java
@@ -0,0 +1,13 @@
+package com.nirSchedular.nirSchedularMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirSchedularMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirSchedularMongoApplication.class, args);
+	}
+
+}
---
+++ nirSchedularMongo/nirSchedularMongo/src/test/java/com/nirSchedular/nirSchedularMongo/NirSchedularMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirSchedular.nirSchedularMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NirSchedularMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
