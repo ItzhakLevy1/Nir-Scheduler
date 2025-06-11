cf36cc1d528aaa303371fa2c849b5c34304f3e92
Itzhak Levy <itzhaklevydev@gmail.com>
2025-06-11 12:45:45 +0300

    Add mobile screen size code for Header and change class names

--- nirSchedularReact/src/components/Header.jsx
+++ nirSchedularReact/src/components/Header.jsx
@@ -9,22 +9,22 @@ const Header = () => {
           א.א. גובה
         </Link>
 
-        <section>
-          <a href="tel:0526126120" className="link-button">
+        <section className="contact-links">
+          <a href="tel:0526126120" className="contact-link">
             📞 חייג אלינו
           </a>
-          <a href="https://wa.me/972526126120" className="link-button">
+          <a href="https://wa.me/972526126120" className="contact-link">
             💬 כתוב לנו
           </a>
           <a
             href="https://ul.waze.com/ul?ll=31.993925,34.764165&navigate=yes&zoom=17"
-            className="link-button"
+            className="contact-link"
           >
             🌏 נווט אלינו
           </a>
         </section>
 
-        <nav className="nav-links">
+        <nav className="order-training">
           <Link to="/booking">הזמנת הדרכה</Link>
         </nav>
       </div>
